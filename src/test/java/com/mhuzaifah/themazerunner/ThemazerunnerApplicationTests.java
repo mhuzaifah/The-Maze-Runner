16d60fae06d0fa20b66b39942a5796c986f62422
Muhammad Huzaifah <mhuzaifah989@gmail.com>
2024-05-17 04:47:16 -0400

    init files

---
+++ src/test/java/com/mhuzaifah/themazerunner/ThemazerunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhuzaifah.themazerunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemazerunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
